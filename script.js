4b9cb0e4ee6050d8070b8943d3126672935dbf2d
AbnerAbreu <52495113+AbnerAbreu@users.noreply.github.com>
2019-08-02 15:48:45 -0300

    Adicionar callback/exercicios/promisse

--- script.js
+++ script.js
@@ -8,28 +8,45 @@
 // }
 // receberNome(abrirAlert);
 
-let lista = document.querySelector("ol");
-let corpo = document.querySelector("body");
+// let lista = document.querySelector("ol");
+// let corpo = document.querySelector("body");
 
 
-let raizes = [
-    {nome:"batata"},
-    {nome:"mandioca"},
-    {nome:"rabanete"},
-];
+// let raizes = [
+//     {nome:"batata"},
+//     {nome:"mandioca"},
+//     {nome:"rabanete"},
+// ];
 
-let imprimirComidas = (cor) =>{
-    for(let raiz of raizes){
-        lista.innerHTML +=`<li>${raiz.nome}</li>`
-        corpo.style.backgroundColor = cor;
+// let imprimirComidas = (cor) =>{
+//     for(let raiz of raizes){
+//         lista.innerHTML +=`<li>${raiz.nome}</li>`
+//         corpo.style.backgroundColor = cor;
 
-    }
+//     }
+// }
+// let adicionarComidas = (cor) =>{
+//     let escolherCor = prompt("escolha uma cor em ingles");
+//     setTimeout(() => {
+//         raizes.push({nome: "gengibre"});
+//         cor(escolherCor);
+//     }, 2000);
+// }
+// adicionarComidas(imprimirComidas)
+
+let diaSemana = "sextou";
+
+let callback = (resolve,reject) =>{
+     if(diaSemana == "sextou"){
+         resolve("acertou");
+        }
+     else{
+         reject("errrouuu");
+     }
 }
-let adicionarComidas = (cor) =>{
-    let escolherCor = prompt("escolha uma cor em ingles");
-    setTimeout(() => {
-        raizes.push({nome: "gengibre"});
-        cor(escolherCor);
-    }, 2000);
-}
-adicionarComidas(imprimirComidas)
+
+let promessa = new Promise(callback);
+
+promessa
+    .then(alert("acertou"))
+    .catch(console.error)
