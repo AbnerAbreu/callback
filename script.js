a0ce5c003b473c15baf25bcb4b5f98114990d4e1
AbnerAbreu <52495113+AbnerAbreu@users.noreply.github.com>
2019-08-02 15:14:06 -0300

    Adicionar callback/exercicios

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function abrirAlert(nome){
-    alert(nome);
-}
+// function abrirAlert(nome){
+//     alert(nome);
+// }
 
-function receberNome(callbackNome){
-    let nome = "Abner"
-    callbackNome(nome);
+// function receberNome(callbackNome){
+//     let nome = "Abner"
+//     callbackNome(nome);
+// }
+// receberNome(abrirAlert);
+
+let lista = document.querySelector("ol");
+let corpo = document.querySelector("body");
+
+
+let raizes = [
+    {nome:"batata"},
+    {nome:"mandioca"},
+    {nome:"rabanete"},
+];
+
+let imprimirComidas = (cor) =>{
+    for(let raiz of raizes){
+        lista.innerHTML +=`<li>${raiz.nome}</li>`
+        corpo.style.backgroundColor = cor;
+
+    }
 }
-receberNome(abrirAlert);
+let adicionarComidas = (cor) =>{
+    let escolherCor = prompt("escolha uma cor em ingles");
+    setTimeout(() => {
+        raizes.push({nome: "gengibre"});
+        cor(escolherCor);
+    }, 2000);
+}
+adicionarComidas(imprimirComidas)
